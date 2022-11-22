d54dba5fdd25c6aff3fac26ce7b0bd485906ae66
yaelx <yaelx@localhost>
2022-11-22 22:26:11 +0100

    add config socket io server

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const passport = require("passport");
 const app = express();
-const io = require("socket.io");
-const http = require("http").Server(app);
+const http = require("http");
 /***
  *  ------------- Socket IO --------------------
  */
-socket = io(http);
-socket.on("connection", (socket) => {
-  console.log("user connected");
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  // quand un user est connecté
+  console.log("a user is connected.");
+
+  // prend l'userId et le socketId de l'user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  // envoie et récupère les messages
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  // quand un user est déconnecté
+  socket.on("disconnect", () => {
+    console.log("a user is disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
   });
 });
 
